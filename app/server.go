30b6a50ff4bcc8e96bfd3a04c27f800073a18766
Yves <yves.as@gmail.com>
2025-02-02 11:49:15 -0300

    improvements

--- app/server.go
+++ app/server.go
@@ -6,22 +6,38 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	host := "0.0.0.0"
+	port := 6379
+	address := fmt.Sprintf("%s:%d", host, port) // joins string and number into one string
+
+	l, err := net.Listen("tcp", address)
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Printf("❌ Failed to bind to address %s: %v\n", address, err)
 		os.Exit(1)
 	}
-	// conn, err = l.Accept()
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close() // ??? Listener date when exiting the function
+	fmt.Printf("✅ Server listening on address %s\n", address)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Printf("⚠️ Error accepting connection: %v\n", err) // err.Error()
+			continue                                               // Keep trying to accept new connections.
+		}
+
+		fmt.Println("✅ Accepted connection.")
+
+		_, writeErr := conn.Write([]byte("+PONG\r\n"))
+		if writeErr != nil {
+			fmt.Printf("⚠️ Failed to answered (write to connection): %v\n", writeErr)
+		} else {
+			fmt.Println("✅ Answered with success.")
+		}
+
+		conn.Close()
 	}
-	fmt.Println("Connected!")
-	conn.Write([]byte("+PONG\r\n"))
 }
